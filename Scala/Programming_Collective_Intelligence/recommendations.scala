89e8a0a3091d098a68595fd8f907b9d0ea16d06d
Stephen Sloan <steve@stevesloan.com>
2011-01-06 16:19:18 -0800

    Added Pearson Correlation Score example implemented in Scala

--- Scala/Programming_Collective_Intelligence/recommendations.scala
+++ Scala/Programming_Collective_Intelligence/recommendations.scala
@@ -53,26 +53,59 @@ object Recommendations {
         )
     )
 
-    import scala.math
+    import scala.math.{pow, sqrt}
 
     def main(args: Array[String])
     {
-        val person1 = "Lisa Rose"
-        val person2 = "Toby"
+        val person1: String = args match {
+            case Array(first, _*) => first
+            case _ => "Lisa Rose"
+        }
+        val person2: String = args match {
+            case Array(_, second, _*) => second
+            case _ => "Gene Seymour"
+        }
+
         print("This is a test of \"sim_distance\" for %s and %s: ".format(person1, person2))
-        val distance: Double = sim_distance(critics, "Lisa Rose", "Toby")
-        println(distance)
+        val d1: Double = sim_distance(critics, person1, person2)
+        println(d1)
+
+        print("This is a test of \"sim_pearson\" for %s and %s: ".format(person1, person2))
+        val d2: Double = sim_pearson(critics, person1, person2)
+        println(d2)
     }
 
     def sim_distance(prefs: Map[String, Map[String, Double]], person1: String, person2: String): Double = {
         val sum_of_squares =
             prefs(person1).filterKeys(prefs(person2).isDefinedAt(_)).foldLeft[Double](0.0){
-                (sum, entry) => sum + math.pow(entry._2 - prefs(person2)(entry._1), 2.0)
+                (sum, entry) => sum + pow(entry._2 - prefs(person2)(entry._1), 2.0)
             }
         if (sum_of_squares == 0.0) {
             0.0
         } else {
-            1.0 / (1.0 + math.sqrt(sum_of_squares))
+            1.0 / (1.0 + sqrt(sum_of_squares))
+        }
+    }
+
+    def sim_pearson(prefs: Map[String, Map[String, Double]], person1: String, person2: String): Double = {
+        val items = prefs(person1).keySet.intersect(prefs(person2).keySet)
+        val iSize = items.size
+        if (iSize == 0) {
+            0.0
+        } else {
+            var sum1, sum2, sum1Sq, sum2Sq, pSum, p1Item, p2Item = 0.0
+            for (item <- items) {
+                p1Item = prefs(person1)(item)
+                p2Item = prefs(person2)(item)
+                sum1 += p1Item
+                sum2 += p2Item
+                sum1Sq += pow(p1Item, 2)
+                sum2Sq += pow(p2Item, 2)
+                pSum += p1Item * p2Item
+            }
+            val num = pSum - ((sum1 * sum2) / iSize)
+            val den = sqrt((sum1Sq - (pow(sum1, 2) / iSize)) * (sum2Sq - (pow(sum2, 2) / iSize)))
+            num / den
         }
     }
 }
